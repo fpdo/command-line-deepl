8b1fefb0963786c47c0447c594419f34e38c476e
Felipe de Oliveira <felipe@deoliveira.dev>
2023-04-14 13:52:52 +0200

    feature: add pytest framework and basic tests
    Signed-off-by: Felipe de Oliveira <felipe@deoliveira.dev>

--- translate.py
+++ translate.py
@@ -9,12 +9,20 @@ class Translator:
         load_dotenv()
         self.api_key = os.getenv("API_KEY")
         self.deepl = deepl.Translator(self.api_key)
+        # This is done mainly so that tests can be run
+        if (len(prompt) == 0):
+            return
+        self.prepare(prompt)
+
+    def prepare(self, prompt):
         if ("-" in prompt[0]):
             src_lang, dst_lang = prompt[0].split("-")
             self.prompt = " ".join(prompt[1:])
             # DeepL API needs the English version to be specificed
-            if dst_lang == "EN" or dst_lang == "en":
+            if (dst_lang == "EN" or dst_lang == "en"):
                 dst_lang += "-US"
+            elif (dst_lang == "PT" or dst_lang == "pt"):
+                dst_lang += "-BR"
             self.request(src_lang.upper(), dst_lang.upper())
         else:
             self.prompt = " ".join(prompt)
@@ -23,7 +31,13 @@ class Translator:
     def request(self, src_lang="EN", dst_lang="NL"):
         rsp = self.deepl.translate_text(self.prompt, source_lang=src_lang,
                                         target_lang=dst_lang)
-        print(rsp.text)
+        self.rsp = rsp.text.lower()
+
+    def get_response(self):
+        return self.rsp
 
 
-obj = Translator(sys.argv[1:])
+query = sys.argv
+if ("pytest" not in query[0]):
+    obj = Translator(query[1:])
+    print(f"\n{obj.get_response()}\n")
